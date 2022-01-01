36208c1e54b38f7e8592c5f7aa88f781d2261358
cpele <geekarist@users.noreply.github.com>
2022-01-01 17:06:04 +0100

    Initial import: view geo intent

---
+++ app/src/main/java/me/cpele/ulcn/MainActivity.kt
@@ -0,0 +1,13 @@
+package me.cpele.ulcn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.text_view).text = intent.toString()
+    }
+}
