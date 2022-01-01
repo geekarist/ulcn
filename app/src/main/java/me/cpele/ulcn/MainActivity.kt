575106c88a6e3e973b47b88d894483ac3b5fb87e
cpele <geekarist@users.noreply.github.com>
2022-01-01 20:53:28 +0100

    Keep only managed links
    squash! Validate links
    
    squash! squash! Validate links
    
    squash! squash! squash! Validate links
    
    squash! squash! squash! squash! Validate links
    
    squash! Keep only managed links
    
    squash! squash! Keep only managed links

--- app/src/main/java/me/cpele/ulcn/MainActivity.kt
+++ app/src/main/java/me/cpele/ulcn/MainActivity.kt
@@ -1,58 +1,90 @@
 package me.cpele.ulcn
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.SpannableString
 import android.text.SpannableStringBuilder
 import android.text.Spanned
 import android.text.TextUtils
 import android.text.style.ClickableSpan
 import android.view.View
 import android.widget.TextView
-import androidx.core.text.buildSpannedString
-import androidx.core.text.inSpans
-import androidx.core.text.set
+import androidx.appcompat.app.AppCompatActivity
 import java.net.URLDecoder
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("QueryPermissionsNeeded")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val textView = findViewById<TextView>(R.id.text_view)
-        textView.text = intent.let {
-            val uri = it.data
-            val encodedUri = uri.toString()
-            val decodedUri = URLDecoder.decode(encodedUri)
-            val q = decodedUri.replace("geo:0,0?q=", "")
-            val links = q.split(' ')
-            val spans = links.map { link: CharSequence ->
-                object : ClickableSpan() {
-                    override fun onClick(widget: View) {
-                        startActivity(
-                            Intent()
-                                .setAction(Intent.ACTION_VIEW)
-                                .setData(Uri.parse(link.toString()))
-                        )
-                    }
-                } to link
-            }.map { (span, link) ->
-                SpannableStringBuilder(link).apply {
-                    setSpan(
-                        span,
-                        0,
-                        link.length,
-                        Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
-                    )
-                }
-            }.map { builder ->
-                builder.apply {
-                    appendln()
-                    appendln()
-                }
-            }
-            TextUtils.concat(*spans.toTypedArray())
-        }
+        textView.text = textViewContents(applicationContext, intent)
     }
 }
+
+fun textViewContents(
+    context: Context,
+    actIntent: Intent
+): CharSequence {
+
+    val uri = actIntent.data
+    val encodedUri = uri.toString()
+
+    @Suppress("DEPRECATION")
+    val decodedUri = URLDecoder.decode(encodedUri)
+    val q = decodedUri.replace("geo:0,0?q=", "")
+    val words = q.split(' ')
+
+    val spans = words.asSequence()
+        .map { link -> convertToIntent(link) }
+        .filter { intent -> isIntentManaged(intent, context.packageManager) }
+        .map { intent ->
+            val span = clickableSpan(context, intent)
+            span to intent
+        }.map { (span, intent) ->
+            spanIntent(span, intent)
+        }.toList()
+
+    val arrayOfSpans = spans.toTypedArray()
+
+    return TextUtils.concat(*arrayOfSpans)
+}
+
+fun convertToIntent(link: String) =
+    Intent()
+        .setAction(Intent.ACTION_VIEW)
+        .setData(Uri.parse(link))
+
+@SuppressLint("QueryPermissionsNeeded")
+fun isIntentManaged(intent: Intent, packageManager: PackageManager) =
+    packageManager
+        .queryIntentActivities(intent, 0)
+        .isNotEmpty()
+
+fun clickableSpan(context: Context, intent: Intent) =
+    object : ClickableSpan() {
+        override fun onClick(widget: View) {
+            context.startActivity(intent)
+        }
+    }
+
+@Suppress("DEPRECATION")
+fun spanIntent(
+    span: Any,
+    intent: Intent
+): CharSequence =
+    SpannableStringBuilder(intent.data.toString())
+        .apply {
+            setSpan(
+                span,
+                0,
+                intent.data.toString().length,
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+            appendln()
+            appendln()
+        }
+
